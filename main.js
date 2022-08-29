f900fea828eed7a04ffeb0301becf13c4f43ebc7
Feras <ferassaleh1980@gmail.com>
2022-08-29 15:59:29 +0200

    add the showMonthDays,previous,and next functions

--- main.js
+++ main.js
@@ -12,22 +12,59 @@ const YearMonthes = [
     'November',
     'December',
 ];
+
+const daysOfWeek = ['Sun', 'Mon', 'Teu', 'Wed', 'Thu', 'Fri', 'Sat'];
+const daysInHeader = document.getElementById('week-days');
 const daysContainer = document.getElementById('days');
 const daysofCurrentMonth = document.getElementsByClassName('day');
 // store the current month in variable
-const currentMonth = new Date().getMonth();
-const currentDay = new Date().getDay();
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
+console.log(currentMonth);
+
+const currentDay = new Date().getDate();
+// const currentDay = 2;
+
 // store the day number of the current month in variable
-const currentDate = new Date().getDate();
-// console.log(currentDay);
+// const firstDay = new Date().getDate();
+
 // console.log(new Date().getDate());
 // console.log(currentMonth);
 
 let currentMonthDays = [];
 
-const addMonthDays = () => {
+function next() {
+    while (daysContainer.firstChild) {
+        daysContainer.removeChild(daysContainer.firstChild);
+    }
+    console.log(daysContainer.children);
+
+    currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    addMonthDays(currentMonth, currentYear);
+}
+
+function previous() {
+    while (daysContainer.firstChild) {
+        daysContainer.removeChild(daysContainer.firstChild);
+    }
+    console.log(daysContainer.children);
+    currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+    currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+    addMonthDays(currentMonth, currentYear);
+}
+
+const addMonthDays = (month, year) => {
+    //get the first day of the month
+    const firstDay = new Date(year, month).getDate();
+    console.log(firstDay);
+
+    // show the year and the month in the header area
+    document.getElementById('showHeader').innerHTML =
+        YearMonthes[month] + ' ' + year;
+
     let i;
-    let singleDay = 0;
+
     if (
         currentMonth === 3 ||
         currentMonth === 5 ||
@@ -35,6 +72,7 @@ const addMonthDays = () => {
         currentMonth === 10
     ) {
         for (i = 0; i < 30; i++) {
+            const singleDay = document.createElement('div');
             const textnode = document.createTextNode(i + 1);
             singleDay.appendChild(textnode);
             singleDay.setAttribute('class', 'day');
@@ -51,20 +89,103 @@ const addMonthDays = () => {
         currentMonth === 9 ||
         currentMonth === 11
     ) {
-        for (i = 0; i < 31; i++) {
+        for (i = 0; i < 35; i++) {
             const singleDay = document.createElement('div');
-            const textnode = document.createTextNode(i + 1);
-            singleDay.appendChild(textnode);
             singleDay.setAttribute('class', 'day');
             singleDay.setAttribute('key', i);
             daysContainer.appendChild(singleDay);
+
             // console.log(singleDay);
         }
+        if (firstDay === 1) {
+            for (i = 1; i < 32; i++) {
+                daysContainer.children[i].innerHTML = i;
+                const styledDay =
+                    daysContainer.children[currentDay].classList.add(
+                        'currentDay'
+                    );
+            }
+            console.log(currentDay);
+        }
+
+        if (firstDay === 2) {
+            for (i = 2; i < 33; i++) {
+                daysContainer.children[i].innerHTML = i - 1;
+                const styledDay =
+                    daysContainer.children[currentDay + 1].classList.add(
+                        'currentDay'
+                    );
+            }
+        }
+        if (firstDay === 3) {
+            for (i = 3; i < 34; i++) {
+                daysContainer.children[i].innerHTML = i - 2;
+                const styledDay =
+                    daysContainer.children[currentDay + 2].classList.add(
+                        'currentDay'
+                    );
+            }
+        }
+        if (firstDay === 4) {
+            for (i = 4; i < 35; i++) {
+                daysContainer.children[i].innerHTML = i - 3;
+                const styledDay =
+                    daysContainer.children[currentDay + 3].classList.add(
+                        'currentDay'
+                    );
+            }
+        }
+        if (firstDay === 5) {
+            for (i = 5; i < 36; i++) {
+                daysContainer.children[i].innerHTML = i - 4;
+                const styledDay =
+                    daysContainer.children[currentDay + 4].classList.add(
+                        'currentDay'
+                    );
+            }
+        }
+        if (firstDay === 6) {
+            for (i = 6; i < 37; i++) {
+                daysContainer.children[i].innerHTML = i - 5;
+                const styledDay =
+                    daysContainer.children[currentDay + 5].classList.add(
+                        'currentDay'
+                    );
+            }
+        }
+        if (firstDay === 0) {
+            for (i = 0; i < 31; i++) {
+                daysContainer.children[i].innerHTML = i;
+                const styledDay =
+                    daysContainer.children[currentDay - 1].classList.add(
+                        'currentDay'
+                    );
+            }
+        }
+
+        // singleDay.setAttribute('class', 'day');
+        // singleDay.setAttribute('key', i);
+        // daysContainer.appendChild(singleDay);
     }
-    const styledDay =
-        daysContainer.children[currentDate - 1].classList.add('currentDay');
 };
 
-addMonthDays();
+addMonthDays(currentMonth, currentYear);
 // console.log(daysContainer);
 // console.log(daysofCurrentMonth);
+// function showCalender(month, year) {
+//     let firstDay = new Date(year, month).getDay();
+//     document.getElementById('showHeader').innerHTML =
+//         YearMonthes[month] + ' ' + year;
+//     console.log(firstDay);
+// }
+
+// function to get how many days in the month
+function daysInMonth(iMonth, iYear) {
+    console.log(32 - new Date(iYear, iMonth, 32).getDate());
+
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+}
+
+// daysInMonth(0, 2022);
+
+// showCalender(currentMonth, currentYear);
